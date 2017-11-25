f8f963f05482730f2b0e1478bcc5853360c8869d
Matthew Knowles <cesium.cs55@gmail.com>
2017-11-25 17:18:19 -0500

    first commit. Temporary workspace.

--- Scrap.py
+++ Scrap.py
@@ -1,10 +1,26 @@
 import tensorflow as tf
 
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+
 sess = tf.Session()
 
-a = tf.placeholder(tf.float32)
-b = tf.placeholder(tf.float32)
-add_node = a + b
+# Variables
+W = tf.Variable([-3], dtype=tf.float32)
+b = tf.Variable([.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
 
-print(sess.run(add_node, {a: 2, b: 10}))
-print(sess.run(add_node, {a: [5, 4], b: [2, 8]}))
+# Nodes
+linear_model = W * x + b
+squared_delta = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_delta)
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+train = optimizer.minimize(loss)
+
+for i in range(1000):
+    sess.run(train, {x: [1, 2, 3, 4], y: [0, -1, -2, -3]})
+
+print(sess.run([W, b]))
